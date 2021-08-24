4010b676ffd2e6fa8eec4b064927bdc1b635da96
patriciajoann <88016824+patriciajoann@users.noreply.github.com>
2021-08-24 14:09:45 +0700

    Add files via upload

---
+++ tugas1.php
@@ -0,0 +1,37 @@
+<?php
+
+    $dataProduk = array(
+    //nama produk, merk, harga, status stok
+    array("Nama Produk" => "Galaxy S21", "Merk" => "Samsung", "Harga" => 19000000, "Status Stok" => "Stok tersedia"),
+    array("Nama Produk" => "Galaxy A71",  "Merk" => "Samsung", "Harga" => 6999000, "Status Stok" => "Stok habis"),
+    array("Nama Produk" => "iPhone 12 Pro Max", "Merk" => "Apple", "Harga" => 20999000, "Status Stok" => "Stok tersedia"),
+    array("Nama Produk" => "iPhone 12 Mini", "Merk" => "Apple", "Harga" => 10999000, "Status Stok" => "Stok habis"),
+
+    );
+
+    echo $dataProduk[0]["Nama Produk"]."<br>";
+    echo $dataProduk[0]["Merk"]."<br>";
+    echo $dataProduk[0]["Harga"]."<br>";
+    echo $dataProduk[0]["Status Stok"]."<br>";
+    echo "<br>";
+    echo $dataProduk[1]["Nama Produk"]."<br>";
+    echo $dataProduk[1]["Merk"]."<br>";
+    echo $dataProduk[1]["Harga"]."<br>";
+    echo $dataProduk[1]["Status Stok"]."<br>";
+    echo "<br>";
+    echo $dataProduk[2]["Nama Produk"]."<br>";
+    echo $dataProduk[2]["Merk"]."<br>";
+    echo $dataProduk[2]["Harga"]."<br>";
+    echo $dataProduk[2]["Status Stok"]."<br>";
+    echo "<br>";
+    echo $dataProduk[3]["Nama Produk"]."<br>";
+    echo $dataProduk[3]["Merk"]."<br>";
+    echo $dataProduk[3]["Harga"]."<br>";
+    echo $dataProduk[3]["Status Stok"]."<br>";
+    echo "<br>";
+
+
+    
+   
+
+?> 
